b7c3078b8fe2f54244b00efc4065367079ce9288
jeeva <m.jeeva4520@gmai.com>
2026-06-04 15:44:53 +0530

    js variable,data types and operators

---
+++ javascript/script.js
@@ -0,0 +1,66 @@
+// var a = 10;
+// let b = 20;
+// const c = 30;
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// var num =10;
+// var str ="hello";
+// var undefined;
+// var boolan =true;
+// var Null =null;
+
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof undefined);
+// console.log(typeof Null);
+
+
+// var arr =[10,20,30];
+// console.log(typeof arr,arr);
+// var  obj ={
+//     name:"jeeva",
+//     dept:["aids","ct"]
+// }
+// console.log(typeof obj,obj)
+//arthimetic operator(+,-,*,%,/)
+var a =10;
+var b =20;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+
+//logical operator(&&,||,!)
+
+var a ="true";
+var b ="false";
+
+console.log(a&&b)
+console.log(a||b)
+
+//relational operator(>,>=,<,<=,==,!=)
+
+a=10;
+b="20";
+console.log(a>b)
+console.log(a==b)
+console.log(a!=b)
+console.log(a<b)
+
+a=2;
+console.log(a**a);
+ 
+var a =10;
+
+console.log(a--);
+console.log(--a);
+
+a =1;
+b =1;
+c =0;
+
+var result = a++ + --b + ++c - ++a + ++b  + c++ + b++ + a;
+console.log(result)
